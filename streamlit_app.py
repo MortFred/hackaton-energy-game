71f9102ed7ce48f4e3df002b748ac916dec06b02
Roman A Sukhanov <sukhanov.ra@gmail.com>
2024-01-15 12:12:51 +0100

    added initiated sliders for energy source

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,19 @@ import streamlit as st
 st.header('hackaton-energy-game')
 
 with st.sidebar:
-    st.text('Initial game parameters')
+    st.subheader('Initial game parameters')
 
-    st.selectbox('Choose a country', ['Norway'])
+    country = st.selectbox('Choose a country', ['Norway'])
+
+    st.text('Energy sources:')
+    coal = st.slider('Coal', 0, 100)
+    gas = st.slider('Gas', 0, 100)
+    oil = st.slider('Oil', 0, 100)
+    nuclear = st.slider('Nuclear', 0, 100)
+    solar = st.slider('Solar', 0, 100)
+    wind = st.slider('Wind', 0, 100)
+    hydro = st.slider('Hydro', 0, 100)
+    wave = st.slider('Wave', 0, 100)
+    tidal = st.slider('Tidal', 0, 100)
 
     st.button('Display curves')
