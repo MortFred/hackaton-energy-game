ddbcefa90f6f32f0402332e6fee79a8010239398
Robert Dibble <robertdibble@live.co.uk>
2024-01-15 14:17:45 +0100

    Add doc strings

--- generation.py
+++ generation.py
@@ -2,6 +2,14 @@ import math
 
 class Generation:
     def __init__(self, const=100, range_=10, peak_time = 0, time_delta=10):
+        """
+        Initialise values for sinusoidal generator
+            Parameters:
+                    const (float, int): Baseline output across day [W]
+                    range_ (float, int): Deviation either side of output across day [W]
+                    peak_time (float, int): Time of day that peak generation occurs [mins]
+                    time_delta (float, int): Time between increments [mins]
+        """
         # generation constants for sine shaped generation curve
         self.const = const
         self.range = range_
@@ -16,5 +24,13 @@ class Generation:
             self.power[self.time_delta*i] = self.const+ self.range*math.cos((self.time_delta*i-self.peak_time)/(60*24)*(2*math.pi))
 
     def get_power(self, time=None):
-        # calculate generation for current time
-        return self.power[time] if time else self.power
+        """
+        Return power generation for given time
+            Parameters:
+                    time (float, int) [Optional: default = None: Time of day [mins] . If None, return entire day.
+            Returns:
+                    power (float, dict): power generation at time provided or dict of time [mins] and generation values.
+        """
+        # return generation for current time
+        power = self.power[time] if time else self.power
+        return power
