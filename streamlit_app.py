ae8e06b25b87a6f71b402672a73ba7ec382687e0
Lars Petter Hauge <larsenhauge@gmail.com>
2024-01-16 10:28:01 +0100

    Use correct attribute value in SolarGenerator

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ df = pd.DataFrame()
 demand = df_demand["demand"]
 t = np.linspace(0, 24, 24 * 7)
 df_prod = pd.DataFrame({"t": t})
-generation_solar = list(SolarGenerator(peak_value=12000).power.values())
+generation_solar = list(SolarGenerator(peak_value=12000).max_power.values())
 
 df_demand = df_demand.set_index("t")
 df_prod = df_prod.set_index("t")
